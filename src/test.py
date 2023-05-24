af4cd5481ba6675aaf0f213ac7fe12ef3c6a7398
Calvin Chen <calvin.chen@asml.com>
2023-05-24 21:14:39 +0800

    update

--- src/test.py
+++ src/test.py
@@ -54,9 +54,9 @@ def main(config):
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser(description='PyTorch Template')
-    args.add_argument('-c', '--config', default=None, type=str,
+    args.add_argument('-c', '--config', default='SIDD_Val/config.json', type=str,
                       help='config file path (default: None)')
-    args.add_argument('-r', '--resume', default=None, type=str,
+    args.add_argument('-r', '--resume', default='SIDD_Val/model_best.pth', type=str,
                       help='path to latest checkpoint (default: None)')
     args.add_argument('-d', '--device', default=None, type=str,
                       help='indices of GPUs to enable (default: all)')
