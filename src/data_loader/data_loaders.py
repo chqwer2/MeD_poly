f2570506ff78e5868ff911108c5128d2e421294b
Calvin Chen <calvin.chen@asml.com>
2023-05-25 11:21:39 +0800

    update

--- src/data_loader/data_loaders.py
+++ src/data_loader/data_loaders.py
@@ -23,6 +23,7 @@ class DataLoader(BaseDataLoader):
 
 
         # self.dataset = benchmark_data(self.data_dir, task=task, transform=None)
+        batch_size = 1
         self.dataset = PolyU()
         validation_split = 0.0
 
