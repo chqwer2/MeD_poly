023118d0a668a3c83002e436e275f15bc1aa13b1
Calvin Chen <calvin.chen@asml.com>
2023-05-25 11:02:17 +0800

    update

--- src/data_loader/data.py
+++ src/data_loader/data.py
@@ -39,8 +39,8 @@ class PolyU(Dataset):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.paths_L = glob("../../../data/denoising/PolyU/gt/*")
-        self.paths_H = glob("../../../data/denoising/PolyU/noisy/*")
+        self.paths_L = glob("../../../../data/denoising/PolyU/gt/*")
+        self.paths_H = glob("../../../../data/denoising/PolyU/noisy/*")
         self.paths_H.sort()
         self.paths_L.sort()
 
