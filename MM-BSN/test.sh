a3e6a2059c4e20ea9eea61ed9f44b5f74f1bb7bb
Calvin Chen <calvin.chen@asml.com>
2023-05-25 15:33:24 +0800

    update

--- MM-BSN/test.sh
+++ MM-BSN/test.sh
@@ -4,6 +4,6 @@
 
 
 
-python test.py -c SIDD -g 0 \
-       --pretrained ./ckpt/SIDD_MMBSN_o_a45.pth \
+python test.py -g 0 \
+       --pretrained ./ckpt/MMBSN_SIDD_o_a45.pth \
        --test_dir ./dataset/test_data
