e6b3a0cf17afe1989e27ecc5f6b381a2baa42062
Calvin Chen <calvin.chen@asml.com>
2023-05-26 14:04:25 +0800

    update

---
+++ maxim/test.sh
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+# train on SIDD
+
+python3 maxim/run_eval.py --task Denoising --ckpt_path ${SIDD_CKPT_PATH} \
+  --input_dir maxim/images/Denoising --output_dir maxim/images/Results --has_target=False
+
+
+
+
+
