07578d501ca3bd2d8748dc708e12c40c92bf2489
Calvin Chen <calvin.chen@asml.com>
2023-05-26 16:17:23 +0800

    update

--- Restormer/Denoising/utils.py
+++ Restormer/Denoising/utils.py
@@ -93,7 +93,7 @@ def add_noise(clean, ntype, sigma=None):
 
     if 'gaussian' in ntype:
         noisy = clean + np.random.normal(0, sigma, clean.shape)
-        return noisy.float()
+        return np.float32(noisy)
 
     elif  ntype == "poisson":
         noisy = poisson(img)
