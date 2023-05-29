4afbcf11efc898ed2bb86512947e872691670da3
Calvin Chen <calvin.chen@asml.com>
2023-05-29 16:23:34 +0800

    PSNR

--- NAFNet/basicsr/demo_polyU.py
+++ NAFNet/basicsr/demo_polyU.py
@@ -99,21 +99,6 @@ def main():
     model = create_model(opt)
 
 
-
-    inp_shape = (3, 256, 256)
-
-    # pip install ptflops
-    from ptflops import get_model_complexity_info
-    FLOPS = 0
-    macs, params = get_model_complexity_info(model, inp_shape, verbose=False, print_per_layer_stat=True)
-
-    # params = float(params[:-4])
-    # MACs (G) in log scale
-    print(params)
-    macs = float(macs[:-4]) + FLOPS / 10 ** 9
-
-    print('mac', macs, params)
-
     psnr_list = []
     ssim_list = []
 
