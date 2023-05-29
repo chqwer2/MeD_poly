72f4b8fbef6235b99587b0923d55d70465513b7a
chqwer2 <ch981129@126.com>
2023-05-29 21:15:01 +0800

    poly

--- Restormer/basicsr/utils/dist_util.py
+++ Restormer/basicsr/utils/dist_util.py
@@ -19,7 +19,7 @@ def init_dist(launcher, backend='nccl', **kwargs):
 
 
 def _init_dist_pytorch(backend, **kwargs):
-    rank = 0 #int(os.environ['LOCAL_RANK']) #os.environ['RANK'])
+    rank = int(os.environ['RANK'])
     num_gpus = torch.cuda.device_count()
     torch.cuda.set_device(rank % num_gpus)
     dist.init_process_group(backend=backend, **kwargs)
